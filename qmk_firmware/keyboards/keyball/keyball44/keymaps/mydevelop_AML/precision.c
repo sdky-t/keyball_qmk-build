927c6aeb54d88a2f92cb944e8399472d3d5cf826
sdky-t <sshal.y11@gmail.com>
2024-12-03 00:25:42 +0900

    Update precision.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/mydevelop_AML/precision.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/mydevelop_AML/precision.c
@@ -1,1 +1,39 @@
+#ifdef PRECISION_ENABLE
 
+#include QMK_KEYBOARD_H
+
+static const uint16_t PROGMEM down_cpi = PRECISION_CPI;
+
+static uint16_t latest_cpi = 1;
+static bool     cpi_state  = false;
+
+// トグルで切り替え
+void precision_toggle(bool pressed) {
+    if (!pressed) {
+        return;
+    }
+    
+    uint16_t current_cpi = keyball_get_cpi();
+    if (!cpi_state || down_cpi != current_cpi) {
+        latest_cpi = current_cpi;
+        keyball_set_cpi(down_cpi);
+        cpi_state = true;
+    } else {
+        keyball_set_cpi(latest_cpi);
+        cpi_state = false;
+    }
+}
+
+// キー押下中だけ切り替え
+void precision_switch(bool pressed) {
+    if (pressed) {
+        if(!cpi_state) {
+            latest_cpi = keyball_get_cpi();
+        }
+        keyball_set_cpi(down_cpi);
+    } else {
+        keyball_set_cpi(latest_cpi);
+    }
+}
+
+#endif
