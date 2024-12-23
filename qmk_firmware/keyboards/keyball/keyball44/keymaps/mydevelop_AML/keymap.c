1addbaaf6299fd31ae44e8ba6695639bba797a00
sdky-t <sshal.y11@gmail.com>
2024-12-24 00:08:20 +0900

    Update keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/mydevelop_AML/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/mydevelop_AML/keymap.c
@@ -132,8 +132,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-    // Auto enable scroll mode when the highest layer is 4
-    keyball_set_scroll_mode(get_highest_layer(state) == 4);
+    // Auto enable scroll mode when the highest layer is 5
+    keyball_set_scroll_mode(get_highest_layer(state) == 5);
     return state;
 }
 
